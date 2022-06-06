18c71d70e8ec41ab61dcbec52a91008781ea6c6d
Karla Solórzano <Karli.02@hotmail.com>
2022-06-06 15:29:44 -0600

    edición datos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -30,7 +30,7 @@ const Formulario = ({cliente,cargando}) => {
             let respuesta
             if(cliente.id){
                 //Editando un registro
-                const url = `http://localhost:4000/clientes/${cliente.id}`
+                const url = `${import.meta.env.VITE_API_URL}/${cliente.id}`
                 respuesta = await fetch(url, {
                     method: 'PUT',
                     body: JSON.stringify(valores),
@@ -41,7 +41,7 @@ const Formulario = ({cliente,cargando}) => {
 
             }else{
                 //Nuevo registro 
-                const url = 'http://localhost:4000/clientes'
+                const url = import.meta.env.VITE_API_URL
                 respuesta = await fetch(url, {
                     method: 'POST',
                     body: JSON.stringify(valores),
