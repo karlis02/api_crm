c8c489bd10e364cd697c8aa0b5942bbbec10be82
Karla Solórzano <Karli.02@hotmail.com>
2022-06-06 12:10:57 -0600

    first commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,16 @@
+import "../styles/Spinner.css"
+
+const spinner = () => {
+    return (
+    <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        </div>
+    )
+}
+
+export default spinner
